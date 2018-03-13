763f159094d347c4b06ba8961e623fc7d1b40eba
Darius Igna <darius.igna@telenav.com>
2018-03-13 13:38:26 +0200

    Add

--- src/exceptions/InvalidBudgetException.java
+++
@@ -1,5 +0,0 @@
-package exceptions;
-
-public class InvalidBudgetException extends Exception {
-	private static final long serialVersionUID = -69637829093235855L;
-}
--- src/exceptions/InvalidTypeException.java
+++
@@ -1,5 +0,0 @@
-package exceptions;
-
-public class InvalidTypeException extends Exception{
-	private static final long serialVersionUID = -69637829093235855L;
-}
