01015384596d4acf70853449ef7f2434f75189a7
Darius Igna <darius.igna@telenav.com>
2018-04-17 12:49:17 +0300

    Integrate

--- src/test/java/addition/BigBangIntegration.java
+++ src/test/java/addition/BigBangIntegration.java
@@ -18,7 +18,7 @@ public class BigBangIntegration {
         c = new MemberController(r);
     }
 
- //   @org.junit.Test
+    @org.junit.Test
     public void addMember() throws Exception {
         c.addMember(new Member("Darius","1"));
         assert(c.getMembers().size() == 1);
@@ -26,26 +26,39 @@ public class BigBangIntegration {
         assert(c.getMembers().size() == 2);
     }
 
-   // @org.junit.Test
+    @org.junit.Test
     public void addEntry() throws Exception {
         c.addMember(new Member("Andreis","3"));
         int res = c.addEntry("income","100","3");
         assert(res == 0);
     }
 
- //   @org.junit.Test
+    @org.junit.Test
     public void getEntries() throws Exception {
         c.addMember(new Member("Andreis","3"));
         c.addEntry("income","100","3");
         c.addEntry("cost","100","3");
-        assert(c.getEntriesForMember(3).size() == 3);
+        assert(c.getEntriesForMember(3).size() == 2);
     }
 
     @org.junit.Test
     public void testAll() throws Exception {
-        addMember();
-        addEntry();
-        getEntries();
+        c.addMember(new Member("Darius","1"));
+        assert(c.getMembers().size() == 1);
+        c.addMember(new Member("Andrei","2"));
+
+        c.addMember(new Member("Andreis","3"));
+        int res = c.addEntry("income","100","3");
+
+
+        c.addMember(new Member("Andreis","3"));
+        c.addEntry("income","100","3");
+        c.addEntry("cost","100","3");
+
+        assert(c.getMembers().size() == 3);
+        assert(c.getEntriesForMember(3).size() == 3);
+        assert (c.allEntries().size() == 3);
+
     }
 
 
